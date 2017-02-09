b08290d2b78afd82827592d9250b94ca875bd7a3
Kalsi13 <mandeep.kalsi@bskyb.com>
2017-02-09 12:20:50 +0000

    Update mongodb_exporter.go

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/dcu/mongodb_exporter/collector"
 	"github.com/dcu/mongodb_exporter/shared"
-    "gopkg.in/yaml.v2"
+        "gopkg.in/yaml.v2"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 )
