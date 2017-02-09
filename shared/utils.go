c468f16dfde95aa4a1279b05e60094a31b5e809c
Kalsi13 <mandeep.kalsi@bskyb.com>
2017-02-09 12:07:20 +0000

    Update utils.go with Load Certificates param

--- shared/utils.go
+++ shared/utils.go
@@ -4,6 +4,9 @@ import (
 	"regexp"
 	"strings"
 	"strconv"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 )
 
 var (
@@ -24,6 +27,26 @@ func SnakeCase(text string) string {
 func ParameterizeString(text string) string {
 	result := parameterizeRegexp.ReplaceAllString(text, "_")
 	return strings.ToLower(result)
+}
+
+// LoadCertificatesFrom returns certificates for a given pem file
+func LoadCertificatesFrom(pemFile string) (*x509.CertPool, error) {
+	caCert, err := ioutil.ReadFile(pemFile)
+	if err != nil {
+		return nil, err
+	}
+	certificates := x509.NewCertPool()
+	certificates.AppendCertsFromPEM(caCert)
+	return certificates, nil
+}
+
+// LoadKeyPairFrom returns a configured TLS certificate
+func LoadKeyPairFrom(pemFile string, privateKeyPemFile string) (tls.Certificate, error) {
+	targetPrivateKeyPemFile := privateKeyPemFile
+	if len(targetPrivateKeyPemFile) <= 0 {
+		targetPrivateKeyPemFile = pemFile
+	}
+	return tls.LoadX509KeyPair(pemFile, targetPrivateKeyPemFile)
 }
 
 func IsVersionGreater(version string, major int, minor int, release int) bool {
